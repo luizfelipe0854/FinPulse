05bd75738aab5e9900c99f1c2033c38a6565dc6a
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2026-03-10 23:34:18 -0300

    feat(pages): add SignUp page

---
+++ src/components/pages/SignUpPage/index.tsx
@@ -0,0 +1,18 @@
+import { SignUp } from "@/components/organisms";
+import FinPulseLogo from "@/assets/FinPulse.svg";
+import { useNavigate } from "react-router-dom";
+
+export const SignUpPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <img
+        src={FinPulseLogo}
+        alt="FinPulse Logo"
+        className="w-32 h-32 cursor-pointer"
+        onClick={() => navigate("/")}
+      />
+      <SignUp />
+    </div>
+  );
+};
