8a2e62b716ecc225bb90ef0ce660333eca26c420
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2026-03-10 22:36:57 -0300

    feat(atoms): add Badge component

---
+++ src/components/atoms/Badge/index.tsx
@@ -0,0 +1,21 @@
+import { Text } from "@/components/atoms";
+
+type BadgeProps = {
+  label: string;
+  variant?: "success" | "danger" | "warning" | "info";
+};
+
+export const Badge = ({ label, variant = "info" }: BadgeProps) => {
+  const styles = {
+    success: "text-[var(--success)]",
+    danger: "text-[var(--danger)]",
+    warning: "text-[var(--warning)]",
+    info: "text-[var(--info)]",
+  };
+
+  return (
+    <Text variant="badge" as="span" moreProps={`px-2 py-1 ${styles[variant]}`}>
+      {label}
+    </Text>
+  );
+};
