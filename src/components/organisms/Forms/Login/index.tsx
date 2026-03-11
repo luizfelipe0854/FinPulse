05f0649528d314b8417ac54f3f39485c0b7f9352
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2026-03-10 23:22:28 -0300

    feat(organisms): add Form Login component

---
+++ src/components/organisms/Forms/Login/index.tsx
@@ -0,0 +1,62 @@
+import { Button, Text, Input } from "@/components/atoms";
+import { useLogin } from "@/hooks/useLogin";
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+  const { handleLogin, isLoadingLogin } = useLogin();
+  const navigate = useNavigate();
+  return (
+    <form
+      className="flex flex-col gap-4 w-full max-w-sm mx-auto"
+      onSubmit={handleLogin}
+      aria-busy={isLoadingLogin}
+    >
+      <div className="flex flex-col gap-1 text-center">
+        <Text as="h1" variant="title">
+          Login
+        </Text>
+        <Text as="p" variant="body">
+          Preencha os campos abaixo para fazer login.
+        </Text>
+      </div>
+      <label className="flex flex-col gap-1">
+        <Text as="span" variant="body">
+          E-mail:
+        </Text>
+        <Input
+          name="email"
+          placeholder="Digite seu e-mail"
+          type="email"
+          autoComplete="email"
+        />
+      </label>
+      <label className="flex flex-col gap-1">
+        <Text as="span" variant="body">
+          Senha:
+        </Text>
+        <Input
+          name="password"
+          placeholder="Digite sua senha"
+          type="password"
+          autoComplete="new-password"
+          minLength={8}
+        />
+      </label>
+      <div className="flex flex-col justify-center items-center w-full">
+        <Button
+          label={isLoadingLogin ? "Entrando..." : "Entrar"}
+          variant="primary"
+          aria-busy={isLoadingLogin}
+          type="submit"
+          disabled={isLoadingLogin}
+          moreProps="w-full h-10"
+        />
+        <Text as="span" variant="body" moreProps="mt-2 cursor-pointer">
+          <a onClick={() => navigate("/cadastro")}>
+            Não possui conta? Cadastre-se
+          </a>
+        </Text>
+      </div>
+    </form>
+  );
+};
