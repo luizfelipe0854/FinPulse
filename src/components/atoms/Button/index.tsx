2f60068b1e79aaab31927aabbebc7fb6bbf1f81d
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2026-03-10 22:40:48 -0300

    feat(atoms): add Button component

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,36 @@
+type ButtonProps = {
+  type?: "button" | "submit" | "reset";
+  label: string;
+  icon?: React.ReactNode;
+  variant?: "primary" | "secondary";
+  moreProps?: string;
+  onClick?: () => void;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button = ({
+  type = "button",
+  label,
+  icon,
+  onClick,
+  variant = "primary",
+  moreProps: moreProps,
+}: ButtonProps) => {
+  const base = `px-4 border-none rounded-lg flex items-center justify-center gap-2 text-[var(--surface)] cursor-pointer`;
+
+  const styles = {
+    primary: "bg-[var(--primary)]  hover:bg-[var(--primary)]/90",
+
+    secondary: "bg-[var(--danger)] hover:bg-[var(--danger)]/90",
+  };
+
+  return (
+    <button
+      type={type}
+      className={`${base} ${styles[variant]} ${moreProps ?? ""}`}
+      onClick={onClick}
+    >
+      {icon && <span className="mr-1">{icon}</span>}
+      {label}
+    </button>
+  );
+};
