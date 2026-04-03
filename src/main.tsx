854a0b9ddf7db55dd7cd332a5dd4eb926a983587
Luiz Azevedo <luizfelipedemeloazevedo@gmail.com>
2026-04-03 20:58:50 -0300

    feat(app): setup global providers (AuthProvider) and toast notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
-import { App } from "./App.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@/styles/index.css";
+import { App } from "@/App.tsx";
+import { AuthProvider } from "@/contexts/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <ToastContainer />
+      <App />
+    </AuthProvider>
   </StrictMode>,
 );
